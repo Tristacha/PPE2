60c87d6da59b586fcccf43669f7e660b6c4b26c8
AntoineCo23 <44968876+AntoineCo23@users.noreply.github.com>
2018-11-12 12:13:02 +0100

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -1,25 +1,58 @@
 <?php
-mysql_connect("localhost", "root", "");
-mysql_select_db("nom_db");
-
+session_start();
+// Connexion a la BDD
+$bdd = 'ppemarie';
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$db = mysqli_connect ($hostname, $username, $password, $bdd);
 if(!empty($_POST['nom']))
 {
-	$password = mysql_real_escape_string(htmlspecialchars($_POST['password']));
-	$confirm = mysql_real_escape_string(htmlspecialchars($_POST['confirm']));
-	if($password == $confirm)
-	{
-		$password = sha1($password);
-		$nom = mysql_real_escape_string(htmlspecialchars($_POST['nom']));
-		$prenom = mysql_real_escape_string(htmlspecialchars($_POST['prenom']));
-		$email = mysql_real_escape_string(htmlspecialchars($_POST['email']));
-		$dob = mysql_real_escape_string(htmlspecialchars($_POST['dob']));
-		$codepostal = mysql_real_escape_string(htmlspecialchars($_POST['codepostal']));
-		$numero = mysql_real_escape_string(htmlspecialchars($_POST['numero']));
-		mysql_query("INSERT INTO Clients VALUES('', '$nom', '$prenom', '$codepostal', '$email', '$dob', '$numero','$password')");
-	}
-	else
-	{
-		echo 'Les deux mots de passe que vous avez rentrés ne correspondent pas…';
-	}
+	$password = mysqli_real_escape_string($db, $_POST['password']);
+	htmlspecialchars($password);
+	$confirm = mysqli_real_escape_string($db, $_POST['confirm']);
+	htmlspecialchars($confirm);
+	$nom = mysqli_real_escape_string($db, $_POST['nom']);
+	htmlspecialchars($nom);
+	$prenom = mysqli_real_escape_string($db, $_POST['prenom']);
+	htmlspecialchars($prenom);
+	$email = mysqli_real_escape_string($db, $_POST['email']);
+	htmlspecialchars($email);
+	$dob = mysqli_real_escape_string($db, $_POST['dob']);
+	htmlspecialchars($dob);
+	$codepostal = mysqli_real_escape_string($db, $_POST['codepostal']);
+	htmlspecialchars($codepostal);
+	$numero = mysqli_real_escape_string($db, $_POST['numero']);
+	htmlspecialchars($numero);
+
+	if (strlen($password)>=6)
+            {
+               if ($password==$confirm)
+                {
+            		// On crypte le mot de passe
+                	$password = sha1($password);
+                	$sql ="INSERT INTO clients VALUES('', '$nom', '$prenom',  '$email','$numero', '$dob', '','$password', '$codepostal')";
+					$res = $db->query($sql);
+					mysqli_close($db);
+					//mysqli_query($db, $querry);
+				}
+				else{
+						echo  
+								"<script>
+									alert(\"Les mots de passe ne sont pas identiques\");
+									history.back();
+								</script>";
+					}
+
+			}
+			else{
+
+					echo 
+						"<script>
+							alert(\"Le mot de passe est trop court !\");
+							history.back();
+						</script>"; 
+				}
+
 }
 ?>
